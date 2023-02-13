2c3fea6834777ae01bb0a9bde20a1a0b4f239b0e
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-02-13 19:40:53 +0000

    Add and write initial queries

---
+++ 1_queries/05_show_user_reservations.sql
@@ -0,0 +1,8 @@
+SELECT reservations.id, title, cost_per_night, reservations.start_date, AVG(property_reviews.rating) AS average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = 1
+GROUP BY properties.id, reservations.id
+ORDER BY start_date
+LIMIT 10;
